5f49964207472ddcd49c1e514c2fcdaa4af690ff
chrizzlewizzle <cvdwal@gmail.com>
2025-07-07 22:30:32 +0200

    Chapter 2 Lesson 4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -24,3 +24,7 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "This is a link", {"href": "https://www.watbenjedan.nl","target": "_blank"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.watbenjedan.nl\" target=\"_blank\">This is a link</a>")
